bbfd71cba522d711630312b7772b46fec3856d1d
RiftyKopling <arsy1429@gmail.com>
2026-05-24 21:23:55 +0700

    fiks banget inimah

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+df = pd.DataFrame({
+#     "Smartphone": pg1_smartphone,
+#     "Harga (Juta Rp)": pg1_harga,
+#     "Rating": pg1_rating,
+#     "RAM (GB)": pg1_ram,
+#     "Storage (GB)": pg1_storage,
+#     "Prosessor (GHz)": pg1_prosesor
+# })
